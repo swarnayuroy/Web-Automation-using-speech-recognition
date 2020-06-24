8453e052733689b5b6e1a8e0d893538fb365d5dc
Swarnayu Roy <44721290+swarnayuroy@users.noreply.github.com>
2020-06-25 01:18:36 +0530

    Add files via upload

---
+++ mytask.py
@@ -0,0 +1,47 @@
+import speech_recognition as sr
+from selenium import webdriver
+
+speech = sr.Recognizer()        #recognising user's  speech
+
+try:
+    #Listen to user
+    with sr.Microphone() as source:
+        print("Say a search engine")
+        audio = speech.listen(source)
+        s_engine = speech.recognize_google(audio)    #we get the search engine
+        print("Engine recognised as", s_engine)
+        
+        print("\nSay what you want to search ? ")
+        audio = speech.listen(source)
+        searchItem = speech.recognize_google(audio)    #we get the searching object
+        print("Word searched:", searchItem)
+        
+    #Working with browser
+    driver = webdriver.Chrome()
+    if(s_engine in ['Google', 'google']):
+        driver.get('https://google.com')
+        searchbox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+        searchbox.send_keys(searchItem)
+        searchButton = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]')
+        searchButton.click()
+    elif(s_engine in ['YouTube', 'Youtube', 'youtube']):
+        '''driver.get('https://youtube.com')
+        searchbox = driver.find_element_by_xpath('//*[@id="search"]')
+        searchbox.send_keys(searchItem)
+        searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+        searchButton.click()'''
+        if(" " in searchItem):
+            searchItem = searchItem.replace(" ", "+")
+        searchItem = "https://www.youtube.com/results?search_query=" + searchItem
+        driver.get(searchItem)
+    elif(s_engine in ['Wikipedia', 'Wiki', 'wikipedia']):
+        driver.get('https://en.wikipedia.org/wiki/Main_Page')
+        searchbox = driver.find_element_by_xpath('//*[@id="searchInput"]')
+        searchbox.send_keys(searchItem)
+        searchButton = driver.find_element_by_xpath('//*[@id="searchButton"]')
+        searchButton.click()
+    else:
+        print("We couldn't recognize the search engine")
+    print("\nExecution successful.")
+except:
+    print("\nService time out !")
